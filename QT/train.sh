f01a919b1b421b4302c5714dfe59445c94cd21bb
artemprisiazhniuk <71759978+artemprisiazhniuk@users.noreply.github.com>
2023-04-28 22:40:11 +0300

    Update train.sh

--- QT/train.sh
+++ QT/train.sh
@@ -5,7 +5,7 @@
 # python QT/src/utils/train-spm.py ../dataset/train.jsonl spm_ru_unigram_32k.model
 # cd ../../QT
 
-python src/train.py --run_id dataset_run --gpu 0
+python src/train.py --run_id dataset_run --gpu 0 --epochs 20
 
 python src/extract.py --model models/dataset_run_20_model.pt --sample_sentences --run_id dataset_general_run --gpu 0
 python src/extract.py --model models/dataset_run_20_model.pt --sample_sentences --run_id dataset_general_run --gpu 0 --entity_id entity_id --summary_data ../data/space_ru/test.json
