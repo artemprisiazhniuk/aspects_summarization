ec370d4c1bbeafbc8ecd4baca2c4c811bf6827c3
artemprisiazhniuk <71759978+artemprisiazhniuk@users.noreply.github.com>
2023-04-28 22:40:46 +0300

    Update train.sh

--- QT/train.sh
+++ QT/train.sh
@@ -7,5 +7,5 @@
 
 python src/train.py --run_id dataset_run --gpu 0 --epochs 20
 
-python src/extract.py --model models/dataset_run_20_model.pt --sample_sentences --run_id dataset_general_run --gpu 0
-python src/extract.py --model models/dataset_run_20_model.pt --sample_sentences --run_id dataset_general_run --gpu 0 --entity_id entity_id --summary_data ../data/space_ru/test.json
+python src/extract.py --model models/dataset_run_10_model.pt --sample_sentences --run_id dataset_general_run --gpu 0
+python src/extract.py --model models/dataset_run_10_model.pt --sample_sentences --run_id dataset_general_run --gpu 0 --entity_id entity_id --summary_data ../data/space_ru/test.json
